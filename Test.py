Fixed opposite stretch and use of angles to radians

Test.py
@@ -1,21 +1,42 @@
 from Transformations import *
+import numpy as np
+import math
 
-initial = (7, 4)
+
+# initial = [0, 5.657]
+#
+# print(rotateAnticlockwise(initial, 0))
+# print(rotateClockwise(initial, 0))
+
+def rotate(angle, initial):
+    newX = (initial[0] * math.cos(angle)) - (initial[1] * math.sin(angle))
+    newY = (initial[1] * math.cos(angle)) + (initial[0] * math.sin(angle))
+    return (newX, newY)
+
+
+# def rotate(angle, initial):
+#     trans = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
+#     initial = np.array([initial[0], initial[1]])
+#     return initial.dot(trans)
+#
+print(rotate(2.64, [4, -4]))
+print(rotate(-2.64, [-1.583981101622533, 5.430562021531719]))
+
 
 # newCoords = scaleObliqueNegative(initial, 1)
 # print(newCoords)
 #
 #
 # newNewCoords = scaleObliqueNegative(newCoords, -0.5)
+# # print(newNewCoords)
+#
+#
+# amount = 7
+# newCoords = rotateClockwise(initial, amount)
+# print(newCoords)
+#
+# newNewCoords = rotateAnticlockwise(newCoords, amount)
 # print(newNewCoords)
-
-
-amount = 7
-newCoords = rotateClockwise(initial, amount)
-print(newCoords)
-
-newNewCoords = rotateAnticlockwise(newCoords, amount)
-print(newNewCoords)
 
 # newCoords = scaleObliqueNegative(initial, 2)
 # print(newCoords)
@@ -24,4 +45,4 @@ print(newNewCoords)
 # newNewCoords = scaleObliqueNegative(newCoords, -0.6666666666)
 # print(newNewCoords)
 
-# TODO: -Rotation doesnt work in reverse mode
+# TODO: - Rotation doesnt work in reverse mode

Transformations.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 
 # 0
@@ -61,75 +62,92 @@ def scaleObliqueNegative(initial, amount):
     distancePerToLine = abs(x + y) / 2
     newX = newY = 0
     if x > (-1 * y):
-        newX = x + ((amount) * distancePerToLine)
-        newY = y + ((amount) * distancePerToLine)
+        newX = x + (amount * distancePerToLine)
+        newY = y + (amount * distancePerToLine)
     else:
-        newX = x - ((amount) * distancePerToLine)
-        newY = y - ((amount) * distancePerToLine)
-    return (newX, newY)
+        newX = x - (amount * distancePerToLine)
+        newY = y - (amount * distancePerToLine)
+    return newX, newY
 
 
 # 8
 def reflectX(initial):
     newy = initial[1] * -1
-    return (initial[0], newy)
+    return initial[0], newy
 
 
 # 9
 def reflectY(initial):
     newx = initial[0] * -1
-    return (newx, initial[1])
+    return newx, initial[1]
 
 
 # A
 def reflectObliquePositive(initial):
     newX = initial[1]
     newY = initial[0]
-    return (newX, newY)
+    return newX, newY
 
 
 # B
 def reflectObliqueNegative(initial):
     newX = -1 * initial[1]
     newY = -1 * initial[0]
-    return (newX, newY)
+    return newX, newY
 
 
 # C
-def rotateClockwise(initial, amount):
-    angle = (math.pi / 15) * amount
-    radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
-    try:
-        theta = math.atan(initial[1] / initial[0])
-    except ZeroDivisionError:
-        theta = 0
-    newX = radius * math.cos(theta + angle)
-    newY = radius * math.sin(theta + angle)
-    return (newX, newY)
+# def rotateClockwise(initial, amount):
+#     angle = (math.pi / 15) * amount
+#     radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
+#     try:
+#         theta = math.atan(initial[1] / initial[0])
+#     except ZeroDivisionError:
+#         theta = 0
+#     if initial[0] < 0 and initial[1] > 0:
+#         theta = 180 - theta
+#     newX = radius * math.cos(theta + angle)
+#     newY = radius * math.sin(theta + angle)
+#     return newX, newY
 
 
 # D
+# def rotateAnticlockwise(initial, amount):
+#     angle = (math.pi / 15) * amount
+#     radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
+#     try:
+#         theta = math.atan(initial[1] / initial[0])
+#     except ZeroDivisionError:
+#         theta = 0
+#     if initial[0] < 0 and initial[1] > 0:
+#         theta = 180 - theta
+#     newX = radius * math.cos(theta - angle)
+#     newY = radius * math.sin(theta - angle)
+#     return newX, newY
+
 def rotateAnticlockwise(initial, amount):
+    angle = (math.pi / 15) * -amount
+    trans = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
+    initial = np.array([initial[0], initial[1]])
+    newPoint = initial.dot(trans)
+    return newPoint[0], newPoint[1]
+
+
+def rotateClockwise(initial, amount):
     angle = (math.pi / 15) * amount
-    radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
-    try:
-        theta = math.atan(initial[1] / initial[0])
-    except ZeroDivisionError:
-        theta = 0
-    newX = radius * math.cos(theta - angle)
-    newY = radius * math.sin(theta - angle)
-    return (newX, newY)
-
-
+    trans = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
+    initial = np.array([initial[0], initial[1]])
+    newPoint = initial.dot(trans)
+    return newPoint[0], newPoint[1]
 # E
 def shiftObliquePositive(initial, amount):
     newx = initial[0] + amount
     newy = initial[1] + amount
-    return (newx, newy)
+    return newx, newy
 
 
 # F
 def shiftObliqueNegative(initial, amount):
     newx = initial[0] - amount
     newy = initial[1] - amount
-    return (newx, newy)
+    return newx, newy
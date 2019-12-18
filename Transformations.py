dc72693571246d9cb10269930fbdf093a25b23d3
Prem Jagadeesh <prem.jagadeesh@icloud.com>
2019-12-18 12:12:29 +0100

    Added reverse parameter to each tranformation for opposites

--- Transformations.py
+++ Transformations.py
@@ -1,167 +1,164 @@
 import math
-import numpy as np
 
 
 # 0
-def translateLeft(initial, amount):
-    newX = initial[0] - amount
-    return (newX, initial[1])
+def translateLeft(initial, amount, opposite):
+    if opposite:
+        return translateRight(initial, amount, False)
+    else:
+        newX = initial[0] - amount
+        return newX, initial[1]
 
 
 # 1
-def translateRight(initial, amount):
-    newX = initial[0] + amount
-    return (newX, initial[1])
+def translateRight(initial, amount, opposite):
+    if opposite:
+        return translateLeft(initial, amount, False)
+    else:
+        newX = initial[0] + amount
+        return newX, initial[1]
 
 
 # 2
-def translateDown(initial, amount):
-    newy = initial[1] - amount
-    return (initial[0], newy)
+def translateDown(initial, amount, opposite):
+    if opposite:
+        return translateUp(initial, amount, False)
+    else:
+        newy = initial[1] - amount
+        return initial[0], newy
 
 
 # 3
-def translateUp(initial, amount):
-    newy = initial[1] + amount
-    return (initial[0], newy)
+def translateUp(initial, amount, opposite):
+    if opposite:
+        return translateDown(initial, amount, False)
+    else:
+        newy = initial[1] + amount
+        return initial[0], newy
 
 
 # 4
-def scaleHorizontal(initial, amount):
-    newx = initial[0] * (amount)
-    return (newx, initial[1])
+def scaleHorizontal(initial, amount, opposite):
+    if opposite:
+        newx = initial[0] * (1 / (amount + 1))
+        return newx, initial[1]
+    else:
+        newx = initial[0] * (amount + 1)
+        return newx, initial[1]
 
 
 # 5
-def scaleVertical(initial, amount):
-    newy = initial[1] * (amount)
-    return (initial[0], newy)
+def scaleVertical(initial, amount, opposite):
+    if opposite:
+        newy = initial[1] * (1 / (amount + 1))
+        return initial[0], newy
+    else:
+        newy = initial[1] * (amount + 1)
+        return initial[0], newy
 
 
 # 6
-def scaleObliquePositive(initial, amount):
+def scaleObliquePositive(initial, amount, opposite):
+    scaleFactor = amount
+    if opposite:
+        scaleFactor = (-1 * (amount / (amount + 1)))
     # 1 - 16 instead of 0 - 15
     x = initial[0]
     y = initial[1]
     distancePerToLine = abs(x - y) / 2
     newX = newY = 0
     if x > y:
-        newX = x + ((amount) * distancePerToLine)
-        newY = y - ((amount) * distancePerToLine)
+        newX = x + (scaleFactor * distancePerToLine)
+        newY = y - (scaleFactor * distancePerToLine)
     else:
-        newX = x - ((amount) * distancePerToLine)
-        newY = y + ((amount) * distancePerToLine)
+        newX = x - (scaleFactor * distancePerToLine)
+        newY = y + (scaleFactor * distancePerToLine)
 
     return (newX, newY)
 
 
 # 7
-def scaleObliqueNegative(initial, amount):
+def scaleObliqueNegative(initial, amount, opposite):
+    scaleFactor = amount
+    if opposite:
+        scaleFactor = (-1 * (amount / (amount + 1)))
     x = initial[0]
     y = initial[1]
     distancePerToLine = abs(x + y) / 2
     newX = newY = 0
     if x > (-1 * y):
-        newX = x + (amount * distancePerToLine)
-        newY = y + (amount * distancePerToLine)
+        newX = x + (scaleFactor * distancePerToLine)
+        newY = y + (scaleFactor * distancePerToLine)
     else:
-        newX = x - (amount * distancePerToLine)
-        newY = y - (amount * distancePerToLine)
+        newX = x - (scaleFactor * distancePerToLine)
+        newY = y - (scaleFactor * distancePerToLine)
     return newX, newY
 
 
 # 8
-def reflectX(initial):
+def reflectX(initial, amount, opposite):
     newy = initial[1] * -1
     return initial[0], newy
 
 
 # 9
-def reflectY(initial):
+def reflectY(initial, amount, opposite):
     newx = initial[0] * -1
     return newx, initial[1]
 
 
 # A
-def reflectObliquePositive(initial):
+def reflectObliquePositive(initial, amount, opposite):
     newX = initial[1]
     newY = initial[0]
     return newX, newY
 
 
 # B
-def reflectObliqueNegative(initial):
+def reflectObliqueNegative(initial, amount, opposite):
     newX = -1 * initial[1]
     newY = -1 * initial[0]
     return newX, newY
 
 
 # C
-# def rotateClockwise(initial, amount):
-#     angle = (math.pi / 15) * amount
-#     radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
-#     try:
-#         theta = math.atan(initial[1] / initial[0])
-#     except ZeroDivisionError:
-#         theta = 0
-#     if initial[0] < 0 and initial[1] > 0:
-#         theta = 180 - theta
-#     newX = radius * math.cos(theta + angle)
-#     newY = radius * math.sin(theta + angle)
-#     return newX, newY
+def rotateClockwise(initial, amount, opposite):
+    if opposite:
+        return rotateAnticlockwise(initial, amount, False)
+    else:
+        return rotate(initial, -amount)
 
 
 # D
-# def rotateAnticlockwise(initial, amount):
-#     angle = (math.pi / 15) * amount
-#     radius = math.sqrt((initial[0] ** 2) + (initial[1] ** 2))
-#     try:
-#         theta = math.atan(initial[1] / initial[0])
-#     except ZeroDivisionError:
-#         theta = 0
-#     if initial[0] < 0 and initial[1] > 0:
-#         theta = 180 - theta
-#     newX = radius * math.cos(theta - angle)
-#     newY = radius * math.sin(theta - angle)
-#     return newX, newY
-
-# def rotateAnticlockwise(initial, amount):
-#     angle = (math.pi / 15) * -amount
-#     trans = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
-#     initial = np.array([initial[0], initial[1]])
-#     newPoint = initial.dot(trans)
-#     return newPoint[0], newPoint[1]
-#
-#
-# def rotateClockwise(initial, amount):
-#     angle = (math.pi / 15) * amount
-#     trans = np.array([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
-#     initial = np.array([initial[0], initial[1]])
-#     newPoint = initial.dot(trans)
-#     return newPoint[0], newPoint[1]
-
-def rotateClockwise(initial, amount):
-    angle = (math.pi / 15) * -amount
-    newX = (initial[0] * math.cos(angle)) - (initial[1] * math.sin(angle))
-    newY = (initial[1] * math.cos(angle)) + (initial[0] * math.sin(angle))
-    return newX, newY
+def rotateAnticlockwise(initial, amount, opposite):
+    if opposite:
+        return rotateClockwise(initial, amount, False)
+    else:
+        return rotate(initial, amount)
 
 
-def rotateAnticlockwise(initial, amount):
+def rotate(initial, amount):
     angle = (math.pi / 15) * amount
     newX = (initial[0] * math.cos(angle)) - (initial[1] * math.sin(angle))
     newY = (initial[1] * math.cos(angle)) + (initial[0] * math.sin(angle))
     return newX, newY
 
+
 # E
-def shiftObliquePositive(initial, amount):
-    newx = initial[0] + amount
-    newy = initial[1] + amount
+def shiftObliquePositive(initial, amount, opposite):
+    amt = amount
+    if opposite:
+        amt = -amount
+    newx = initial[0] + amt
+    newy = initial[1] + amt
     return newx, newy
 
 
 # F
-def shiftObliqueNegative(initial, amount):
-    newx = initial[0] - amount
-    newy = initial[1] - amount
+def shiftObliqueNegative(initial, amount, opposite):
+    amt = amount
+    if opposite:
+        amt = -amount
+    newx = initial[0] - amt
+    newy = initial[1] - amt
     return newx, newy
